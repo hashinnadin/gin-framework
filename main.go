cac526817bd761633c2d72a8b057607045fe4bc7
hashinnadin <hashinnadin@gmail.com>
2026-03-06 16:16:36 +0530

    add

--- main.go
+++ main.go
@@ -14,58 +14,37 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
+// // handler function
+// func handler(w http.ResponseWriter, r *http.Request) {
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		fmt.Println( "GET request received")
 
-	w.Header().Set("Content-Type", "application/json")
+// 	} else if r.Method == http.MethodPost {
+// 		fmt.Fprintln(w, "POST request received")
 
-	response := map[string]string{
-		"message": "Hello, World!",
+// 	} else {
+// 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+// 	}
+// }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "Request receved")
+	} else if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "succeefullu posted")
+	} else {
+		http.Error(w, "method not setteed", http.StatusMethodNotAllowed)
 	}
-
-	json.NewEncoder(w).Encode(response)
-}
-
-// POST /api/user
-func userHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var user User
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Println("User received:", user.Name, user.Age)
-
-	w.Header().Set("Content-Type", "application/json")
-
-	response := map[string]string{
-		"status": "User received successfully",
-	}
-
-	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
 
-	http.HandleFunc("/api/hello", helloHandler)
-	http.HandleFunc("/api/user", userHandler)
+	http.HandleFunc("/api", handler)
 
 	fmt.Println("Server running on port 8080")
 
