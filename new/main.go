2aa6cbbd26ff6026d2e03b9b9a6b9b9c968ed026
hashinnadin <hashinnadin@gmail.com>
2026-03-09 16:34:37 +0530

    update

--- new/main.go
+++ new/main.go
@@ -47,32 +47,33 @@ type User struct {
 
 var users = []User{
 	{ID: 1, Name: "Hashin"},
-	{ID: 2, Name: "Rahul"},
+	{ID: 2, Name: "Shahala"},
 }
 
 func main() {
-
 	router := gin.Default()
-
 	api := router.Group("/api")
 	{
 		api.GET("/users", getUsers)
 		api.POST("/users", createUser)
 	}
-
 	router.Run(":8080")
 }
 
 func getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
-
 func createUser(c *gin.Context) {
 	var newUser User
 
-	c.BindJSON(&newUser)
+	err := c.ShouldBindJSON(&newUser)
 
+	if err != nil {
+		c.JSON(400, gin.H{
+			"Error": "Invalid json",
+		})
+		return
+	}
 	users = append(users, newUser)
-
-	c.JSON(http.StatusCreated, newUser)
+	c.JSON(201, newUser)
 }
