196bd66ad6d07a0b9e264e1b726f407c596a009e
hashinnadin <hashinnadin@gmail.com>
2026-03-09 20:16:12 +0530

    up

--- API/main.go
+++ API/main.go
@@ -1,45 +1,51 @@
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// type User struct {
+// 	ID   int    `json:"id"`
+// 	Name string `json:"name"`
+// }
+
+// var users = []User{
+// 	{ID: 1, Name: "Hashin"},
+// 	{ID: 2, Name: "Rahul"},
+// }
+
+// func main() {
+
+// 	router := gin.Default()
+
+// 	api := router.Group("/api")
+// 	{
+// 		api.GET("/users", getUsers)
+// 		api.POST("/users", createUser)
+// 	}
+
+// 	router.Run(":8080")
+// }
+
+// func getUsers(c *gin.Context) {
+// 	c.JSON(http.StatusOK, users)
+// }
+
+// func createUser(c *gin.Context) {
+
+// 	var newUser User
+
+// 	c.BindJSON(&newUser)
+
+// 	users = append(users, newUser)
+
+// 	c.JSON(http.StatusCreated, newUser)
+// }
+
 package main
-
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-var users = []User{
-	{ID: 1, Name: "Hashin"},
-	{ID: 2, Name: "Rahul"},
-}
 
 func main() {
 
-	router := gin.Default()
-
-	api := router.Group("/api")
-	{
-		api.GET("/users", getUsers)
-		api.POST("/users", createUser)
-	}
-
-	router.Run(":8080")
-}
-
-func getUsers(c *gin.Context) {
-	c.JSON(http.StatusOK, users)
-}
-
-func createUser(c *gin.Context) {
-
-	var newUser User
-
-	c.BindJSON(&newUser)
-
-	users = append(users, newUser)
-
-	c.JSON(http.StatusCreated, newUser)
 }
--- new/main.go
+++ new/main.go
@@ -32,6 +32,52 @@
 // 	c.JSON(http.StatusOK, products)
 // }
 
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// type User struct {
+// 	ID   int    `json:"id"`
+// 	Name string `json:"name"`
+// }
+
+// var users = []User{
+// 	{ID: 1, Name: "Hashin"},
+// 	{ID: 2, Name: "Shahala"},
+// }
+
+// func main() {
+// 	router := gin.Default()
+// 	api := router.Group("/api")
+// 	{
+// 		api.GET("/users", getUsers)
+// 		api.POST("/users", createUser)
+// 	}
+// 	router.Run(":8080")
+// }
+
+// func getUsers(c *gin.Context) {
+// 	c.JSON(http.StatusOK, users)
+// }
+// func createUser(c *gin.Context) {
+// 	var newUser User
+
+// 	err := c.ShouldBindJSON(&newUser)
+
+// 	if err != nil {
+// 		c.JSON(400, gin.H{
+// 			"Error": "Invalid json",
+// 		})
+// 		return
+// 	}
+// 	users = append(users, newUser)
+// 	c.JSON(201, newUser)
+// }
+
 package main
 
 import (
@@ -40,40 +86,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-var users = []User{
-	{ID: 1, Name: "Hashin"},
-	{ID: 2, Name: "Shahala"},
-}
-
 func main() {
 	router := gin.Default()
-	api := router.Group("/api")
-	{
-		api.GET("/users", getUsers)
-		api.POST("/users", createUser)
-	}
+
+	router.POST("/login", loginHandler)
 	router.Run(":8080")
 }
 
-func getUsers(c *gin.Context) {
-	c.JSON(http.StatusOK, users)
+var data struct {
+	Username string `json:"username"`
+	Password int    `json:"password"`
 }
-func createUser(c *gin.Context) {
-	var newUser User
 
-	err := c.ShouldBindJSON(&newUser)
+func loginHandler(c *gin.Context) {
+
+	err := c.ShouldBindJSON(&data)
 
 	if err != nil {
-		c.JSON(400, gin.H{
-			"Error": "Invalid json",
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request",
 		})
 		return
 	}
-	users = append(users, newUser)
-	c.JSON(201, newUser)
+	if data.Username == "admin" && data.Password == 1234 {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Successfully login",
+		})
+		return
+	}
+	c.JSON(http.StatusUnauthorized, gin.H{
+		"error": "invalid username or password",
+	})
 }
